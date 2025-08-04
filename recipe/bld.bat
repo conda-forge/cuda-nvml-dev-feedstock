0f332df05526ffac137775b10ba1443faadce3a5
Billy Suh <bsuh@nvidia.com>
2025-08-04 13:45:37 -0700

    Update for CUDA 13.0.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,8 @@
 if not exist %PREFIX% mkdir %PREFIX%
+@echo on
 
-move lib\x64\* %LIBRARY_LIB%
-move include\* %LIBRARY_INC%
+REM robocopy returns 0-7 for various successful conditions
+robocopy lib\x64 %LIBRARY_LIB%\x64 /MOVE /E
+if %ERRORLEVEL% GEQ 8 exit 1
+robocopy include %LIBRARY_INC% /MOVE /E
+if %ERRORLEVEL% GEQ 8 exit 1
