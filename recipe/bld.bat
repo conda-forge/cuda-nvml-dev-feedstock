c4b409854d95f0ef0f385e5a6755ade93ce8c46e
Christian Sullivan <chrisullivan@nvidia.com>
2026-09-09 16:21:13 -0400

    Update for CUDA 13.4.1

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,14 @@
+if "%TARGET_PLATFORM%" == "win-arm64" (
+    set CUDA_ARCH=arm64
+) else (
+    set CUDA_ARCH=x64
+)
+
 if not exist %PREFIX% mkdir %PREFIX%
 @echo on
 
 REM robocopy returns 0-7 for various successful conditions
-robocopy lib\x64 %LIBRARY_LIB%\x64 /MOVE /E
+robocopy lib\%CUDA_ARCH% %LIBRARY_LIB%\%CUDA_ARCH% /MOVE /E
 if %ERRORLEVEL% GEQ 8 exit 1
 robocopy include %LIBRARY_INC% /MOVE /E
 if %ERRORLEVEL% GEQ 8 exit 1
